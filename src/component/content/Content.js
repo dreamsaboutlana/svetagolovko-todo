9335ac50780dd055568c3ba9b5289344f3a6641b
Sveta Golovko <dreamsaboutlana@gmail.com>
2017-11-24 12:59:11 +0200

    Added <UserList/> component to Content

--- src/component/content/Content.js
+++ src/component/content/Content.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Greeting } from '../greeting/Greeting';
 import { Numbers } from '../numbers/Numbers';
 import { Counter } from '../counter/Counter';
+import { UsersList } from '../usersList/UsersList';
 
 import './content.scss'
 
@@ -15,6 +16,15 @@ export const Content = () => (
     <Greeting name='Sveta'/>
     <Numbers from={5} to={7} even />
     <Counter />
+    <UsersList
+      users={
+        [
+          { name:'Poul', fname:'Willson', age: 23},
+          { name:'Sarah', fname:'Tomson', age: 18},
+          { name:'Andrey', fname:'Jons', age: 56 }
+        ]
+      }
+    />
   </main>
 );
 
