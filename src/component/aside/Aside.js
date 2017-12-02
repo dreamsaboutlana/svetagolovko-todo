286a7cb74fb6b4e3c69079c5eea3ffb5f8469726
Sveta Golovko <dreamsaboutlana@gmail.com>
2017-12-02 13:18:55 +0200

    Changing ban component

--- src/component/aside/Aside.js
+++ src/component/aside/Aside.js
@@ -5,24 +5,14 @@ import './aside.scss';
 export class Aside extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      active: false
-    };
   }
-
-  changeClass = () => {
-    this.setState({ active: !this.state.active });
-  };
 
   render() {
     return (
       <aside className="aside">
-
-        <Button title={'I\'m smart button'}/>
-          {/*title={'I\'m smart button'}*/}
-          {/*active={this.state.active ? 'active' : ''}*/}
-          {/*onClick={this.changeClass}*/}
-
+        <Button
+          title={'I\'m smart button'}
+        />
       </aside>
     );
   }
